a36c32438bcdcb6e71e4645cc8be6deb3f1452eb
Jan Hršak (jhrsak) <jhrsak@tvz.hr>
2026-03-20 11:55:31 +0100

    Initial commit

---
+++ Backend/src/test/java/hr/tvz/nppjj/studybuddy/StudyBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.tvz.nppjj.studybuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyBuddyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
