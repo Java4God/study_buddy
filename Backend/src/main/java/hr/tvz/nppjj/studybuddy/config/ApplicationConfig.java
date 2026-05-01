8d555963670b3ea9e21095e1f75a967a9e4b8137
Jan Hršak (jhrsak) <jhrsak@tvz.hr>
2026-05-01 13:53:54 +0200

    updated App config

--- Backend/src/main/java/hr/tvz/nppjj/studybuddy/config/ApplicationConfig.java
+++ Backend/src/main/java/hr/tvz/nppjj/studybuddy/config/ApplicationConfig.java
@@ -28,8 +28,6 @@ public class ApplicationConfig{
                         .requestMatchers("/users/login").permitAll()
                         .requestMatchers("/users/register-user").permitAll()
                         .requestMatchers("/users/refresh").permitAll()
-                        .requestMatchers("/users/user-by-id").authenticated()
-                        .requestMatchers("/users/me").authenticated()
                         .requestMatchers("/users").hasRole("ADMIN")
                         .requestMatchers("/users/**").authenticated()
                         .requestMatchers("/password-reset/**").permitAll()
