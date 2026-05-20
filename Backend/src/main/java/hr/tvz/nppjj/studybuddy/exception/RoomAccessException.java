deb0c626cd54979afc02fac8fb97889833202915
abatu12 <andrej.baturina@gmail.com>
2026-05-20 08:39:17 +0100

    feat: exception handlers for study room errors

---
+++ Backend/src/main/java/hr/tvz/nppjj/studybuddy/exception/RoomAccessException.java
@@ -0,0 +1,7 @@
+package hr.tvz.nppjj.studybuddy.exception;
+
+public class RoomAccessException extends RuntimeException {
+  public RoomAccessException(String message) {
+    super(message);
+  }
+}
